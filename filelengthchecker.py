1732cc1c9e46e0ee25a80c1a13cc7d5cecf38f72
Jeevan James <jeevan.james@trocaire.org>
2019-06-06 09:39:39 +0100

    Function to check file length

--- filelengthchecker.py
+++ filelengthchecker.py
@@ -1,1 +1,9 @@
 import os
+length = 10
+for dirpath, dirnames, filenames in os.walk('C:\\Users\\jeevan.james\\Desktop\\new test folder one'):
+    if len(dirpath) > length:
+        print(dirpath)
+    if len(filenames):
+        for file in filenames:
+            if len(file) > length:
+                print(os.path.join(dirpath,file))
